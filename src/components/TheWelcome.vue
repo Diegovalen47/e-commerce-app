77c8156850b575a786f0349b04f0f35ea9b7e91b
San Valen <osoriovalentin5@gmail.com>
2024-03-01 14:04:25 -0500

    Add shadcn-vue configuration with tailwind

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -5,9 +5,42 @@ import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
+import { Button } from '@/components/ui/button'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
 </script>
 
 <template>
+  <div class="text-3xl font-bold underline">
+    hola
+    <AlertDialog>
+    <AlertDialogTrigger>
+      <Button>Click me</Button>
+    </AlertDialogTrigger>
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+        <AlertDialogDescription>
+          This action cannot be undone. This will permanently delete your account
+          and remove your data from our servers.
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogAction>Continue</AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+  </div>
   <WelcomeItem>
     <template #icon>
       <DocumentationIcon />
